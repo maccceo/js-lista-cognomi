3ead21e72cbd403a6f7639348af7e462bdc54b44
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-03 15:49:37 +0200

    Definizione step procedurali

---
+++ javascripts/main.js
@@ -0,0 +1,30 @@
+//Chiedi all’utente il cognome, inseriscilo in un array con altri cognomi e stampa la lista ordinata alfabeticamente.
+//Scrivi a che posizione della lista il nuovo utente si trova
+
+
+var database = [
+	"Piccini",
+	"Alessandri",
+	"Zaniboni",
+	"Tencono",
+	"Chiodoni",
+	"Frizzante",
+	"Halbert";
+	"Francofono",
+	"Pindarico",
+	"Mazzetto",
+	"Bulloni",
+	"Pastore"
+];
+
+
+//pusho il cognome inserito dall'utente nell'arrey con tutti i cognomi
+
+
+//ordine alfabeticamente i cognomi
+
+
+//stampo la posizione del cognome inserito nel nuovo ordine alfabetico
+
+
+//stampo la lista alfabetica intera in un <ul>
